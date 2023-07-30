ebacda8461eabebc9721aa60cc1bd09a7318221f
Nibby <nibby@null.net>
2023-07-31 00:45:41 +1200

    Base commit

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace MauiApp1;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new NavigationPage(new MainPage());
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+        
+        window.Deactivated += HandleWindowDeactivated;
+        
+        return window;
+    }
+
+    private async void HandleWindowDeactivated(object sender, EventArgs e)
+    {
+        await MainThread.InvokeOnMainThreadAsync(async () =>
+        {
+            // Add this delay to increase chances that Android saves fragment state and reject the upcoming PopAsync call()
+            await Task.Delay(5000);
+            
+            Console.WriteLine("About to POP after stop");
+            
+            await Current!.MainPage!.Navigation.PopAsync();
+            
+            Console.WriteLine("Finished POP after stop");
+        });
+    }
+}
---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void PushPage(object sender, EventArgs e)
+    {
+        _ = MainThread.InvokeOnMainThreadAsync(() =>
+        {
+            Navigation.PushAsync(new PushedPage());
+        });
+    }
+}
